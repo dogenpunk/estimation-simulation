32116b0073cacea7fc4832f0fbeeabcd7b83ed34
Christopher Redinger <redinger@gmail.com>
2013-02-06 22:10:12 -0500

    Clojure beta 10

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/redinger/estimation-simulation"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-beta9"]
+  :dependencies [[org.clojure/clojure "1.5.0-beta10"]
                  [org.clojure/data.csv "0.1.2"]
                  [org.apache.commons/commons-math3 "3.1"]]
   :main estimation.simulation)
